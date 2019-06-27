8e465b31fca0359a88050ba27db7b16766921db3
timsuchanek <tim.suchanek@gmail.com>
2019-06-27 11:16:46 +0200

    ensure latest version

--- lift/publish/publish.sh
+++ lift/publish/publish.sh
@@ -6,11 +6,16 @@ git clone https://github.com/prisma/lift
 cd lift
 yarn
 yarn test
+# make sure we're having the latest version
+VERSION=$(npm info @prisma/lift --json | jq .version)
+tmp=$(mktemp)
+jq ".version = \"${VERSION}\"" package.json > "$tmp" && mv "$tmp" package.json
+
 yarn publish --patch --no-git-tag-version
 
 git config --global user.email "prismabots@gmail.com"
 git config --global user.name "prisma-bot"
-export VERSION=`cat package.json | jq .version`
-git commit -a -m $VERSION
+export NEW_VERSION=$(cat package.json | jq .version)
+git commit -a -m $NEW_VERSION
 git remote add origin-push https://${GH_TOKEN}@github.com/prisma/lift.git > /dev/null 2>&1
 git push --quiet --set-upstream origin-push $branch
