2edc2fd20e4d2fba3d50520982b2a76fe5203b2b
timsuchanek <tim.suchanek@gmail.com>
2019-06-27 11:21:29 +0200

    add jq

--- lift/publish/publish.sh
+++ lift/publish/publish.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ex
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
+curl -o /usr/local/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
+chmod +x /usr/local/bin/jq
 env
 git clone https://github.com/prisma/lift
 cd lift
