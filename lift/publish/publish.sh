663c6ba3508ead24a8ce28713dd4688e9555a7b4
timsuchanek <tim.suchanek@gmail.com>
2019-06-27 11:00:10 +0200

    update lift publish script

--- lift/publish/publish.sh
+++ lift/publish/publish.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-
 set -ex
+echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
 env
 git clone https://github.com/prisma/lift
 cd lift
 yarn
 yarn test
-# yarn publish --
+yarn publish --patch --no-git-tag-version
+
+git config --global user.email "prismabots@gmail.com"
+git config --global user.name "prisma-bot"
+git commit -a -m "${cat package.json | jq .version}"
+git remote add origin-push https://${GH_TOKEN}@github.com/prisma/lift.git > /dev/null 2>&1
+git push --quiet --set-upstream origin-push $branch
