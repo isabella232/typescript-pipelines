ead01c7be2f66961aaa0da221d4bca26027e7ce9
timsuchanek <tim.suchanek@gmail.com>
2019-06-28 16:53:45 +0200

    cleanup

--- prisma2-cli/publish/publish.sh
+++ prisma2-cli/publish/publish.sh
@@ -6,7 +6,6 @@ echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
 wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
 mv jq-linux64 /usr/local/bin/jq
 chmod +x /usr/local/bin/jq
-env
 
 git clone https://github.com/prisma/prisma2-cli
 cd prisma2-cli
